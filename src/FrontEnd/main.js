299add14dfa97882352288c5dedad715846a8993
l4tn <matheus.dias17@fatec.sp.gov.br>
2022-05-23 01:56:24 -0300

    refactor: FrontEnd JSON

--- src/FrontEnd/main.js
+++ src/FrontEnd/main.js
@@ -1,18 +1,21 @@
 const api_url = 'http://localhost:8080/locations'
   //javascript placeholder
 
-var places = [];
+var places = []; 
+var places2 = [{"name":"Vittoria","loc":[36.95374,14.53318],"pop":62747},{"name":"Trapani","loc":[38.01604,12.54443],"pop":70654},{"name":"Siracusa","loc":[37.08515,15.273],"pop":123768},{"name":"Sciacca","loc":[37.50824,13.08805],"pop":41023},{"name":"Sardinia","loc":[40,9],"pop":1670219},{"name":"Rossano","loc":[39.5751,16.6349],"pop":38123},{"name":"Rende","loc":[39.33199,16.18439],"pop":35376},{"name":"Ragusa","loc":[36.92824,14.71719],"pop":73333},{"name":"Partinico","loc":[38.04395,13.12004],"pop":31852}];
+
 
 async function GetLocation() {
   const response = await fetch(api_url)
   const stored_Json = await response.json()
   console.log(stored_Json)
   
-  //console.log(ConvertJsonToArray(stored_Json))
-  //return ConvertJsonToArray(stored_Json)
+  console.log(ConvertJsonToArray(stored_Json))
 
   places = stored_Json;
   console.log(places);
+
+  return ConvertJsonToArray(stored_Json)
 }
 
 // Convert the Json to an array
@@ -51,9 +54,11 @@ async function PostLocation() {
   const response = await fetch(api_url, {
     method: 'POST',
     headers: {}, //O erro de CORS ocorria quando preenchia o Headers com Access-Control-Allow-Origin e Content-Type-application/json
+
+    //Convertemos antes os valores para não dar erro de tipo
     body: JSON.stringify(
       {
-        ID: 90,
+        ID: 0,
         Name: name,
         Loc: [lat, lng]
       }
